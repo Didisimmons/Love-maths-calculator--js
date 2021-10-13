d58675ddb6f52bb36346143936146b4a7dedb51e
Didisimmons <onyema.onyejekwe@gmail.com>
2021-10-13 02:06:08 +0000

    setup stucture for game functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Connected!");
+function runGame();
+
+function checkAnswer();
+
+function calculateCorrectAnswer();
+
+function incrementScore();
+
+function incrementWrongScore();
+
+function displayAdditionQuestion();
+
+function displaySubtractQuestion();
+
+function displayMultiplyQuestion();
+
+function displayDivideQuestion();
