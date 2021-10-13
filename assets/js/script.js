85ee6d451ce272c9b70b43ba1adb84b005ac52ec
Didisimmons <onyema.onyejekwe@gmail.com>
2021-10-13 23:04:01 +0000

    add division code to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown" , function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 
 });
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = ""; 
+    document.getElementById("answer-box").focus();  
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -32,11 +41,14 @@ function runGame(gameType) {
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1,num2);
     } else if (gameType === "subtract"){
-        displaySubtractQuestion(num1,num2)
-    }else {
+        displaySubtractQuestion(num1,num2);
+    } else if (gameType === "division"){
+        displayDivideQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 /**
@@ -53,7 +65,7 @@ function checkAnswer() {
         incrementScore();
 
     } else {
-        alert(`Incorrect answer ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);  
+        alert(`Incorrect answer ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);   //informs user of correct answer 
         incrementWrongAnswer();
     }
 
@@ -76,10 +88,12 @@ function calculateCorrectAnswer() {
     if (operator === "+"){
         return [operand1 + operand2, "addition"]; 
     } else if (operator === "x"){
-        return [operand1 * operand2, "multiply"];
+        return [operand1 * operand2,"multiply"];
     } else if (operator === "-"){
-        return [operand1 - operand2, "subtract"];
-    }else {
+        return [operand1 - operand2,"subtract"];
+    } else if (operator === "/"){
+        return [operand1 / operand2,"division"];
+    } else {
         alert(`Unimplemented operator : ${operator}`);
         throw `Unimplemented operator : ${operator}. Aborting!`;
     }
@@ -90,7 +104,7 @@ function calculateCorrectAnswer() {
  */
 function incrementScore() {
     let oldscore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldscore; 
+    document.getElementById("score").innerText = ++oldscore;  //updating score 
   
 }
 
@@ -101,7 +115,7 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
     let oldscore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldscore;  
+    document.getElementById("incorrect").innerText = ++oldscore;   
 
 }
 
@@ -114,10 +128,11 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
     document.getElementById('operator').textContent = "-";
+    
+
 }
 
 function displayMultiplyQuestion(operand1,operand2) {
@@ -126,6 +141,8 @@ function displayMultiplyQuestion(operand1,operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion() {
-    
+function displayDivideQuestion(operand1,operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById('operator').textContent = "/";
 }
