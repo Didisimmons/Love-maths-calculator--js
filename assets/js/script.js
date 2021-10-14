5141574485bfa86a4a7b3e72987d6c580c39d091
Didisimmons <onyema.onyejekwe@gmail.com>
2021-10-14 09:13:53 +0000

    modify division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,7 @@ function checkAnswer() {
         incrementScore();
 
     } else {
-        alert(`Incorrect answer ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);   //informs user of correct answer 
+        alert(`Incorrect answer ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);    
         incrementWrongAnswer();
     }
 
@@ -104,7 +104,7 @@ function calculateCorrectAnswer() {
  */
 function incrementScore() {
     let oldscore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldscore;  //updating score 
+    document.getElementById("score").innerText = ++oldscore; 
   
 }
 
@@ -142,7 +142,7 @@ function displayMultiplyQuestion(operand1,operand2) {
 }
 
 function displayDivideQuestion(operand1,operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById('operand1').textContent = (operand1 * operand2) ;
+    document.getElementById('operand2').textContent = operand2 ;
     document.getElementById('operator').textContent = "/";
 }
